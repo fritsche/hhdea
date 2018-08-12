adf51b8da7f4514fcc4f6599a1036e71944f9503
Gian Fritsche <gian.fritsche@gmail.com>
2018-08-12 19:14:48 -0300

    SPEA2SDE: lack testing

--- HHdEA/src/main/java/br/ufpr/inf/cbio/hhdea/algorithm/SPEA2SDE/SPEA2SDE.java
+++ HHdEA/src/main/java/br/ufpr/inf/cbio/hhdea/algorithm/SPEA2SDE/SPEA2SDE.java
@@ -16,107 +16,116 @@
  */
 package br.ufpr.inf.cbio.hhdea.algorithm.SPEA2SDE;
 
-import java.util.ArrayList;
-import java.util.List;
-import org.uma.jmetal.algorithm.Algorithm;
+import org.uma.jmetal.algorithm.impl.AbstractGeneticAlgorithm;
 import org.uma.jmetal.operator.CrossoverOperator;
 import org.uma.jmetal.operator.MutationOperator;
 import org.uma.jmetal.operator.SelectionOperator;
 import org.uma.jmetal.problem.Problem;
 import org.uma.jmetal.solution.Solution;
-import org.uma.jmetal.util.SolutionListUtils;
+import org.uma.jmetal.util.evaluator.SolutionListEvaluator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
- * @author Gian Fritsche <gmfritsche@inf.ufpr.br>
+ * @author Gian Fritsche <gian.fritsche at gmail.com>
  * @param <S>
  */
-public class SPEA2SDE<S extends Solution> implements Algorithm<List<S>> {
+public class SPEA2SDE<S extends Solution<?>> extends AbstractGeneticAlgorithm<S, List<S>> {
 
-    public static final int TOURNAMENTS_ROUNDS = 1;
-    String step_path = null;
-    int step_value = 0;
-    int populationSize, archiveSize, maxEvaluations, evaluations;
-    CrossoverOperator<S> crossoverOperator;
-    MutationOperator<S> mutationOperator;
-    SelectionOperator<List<S>, S> selectionOperator;
-    List<S> solutionSet, archive, offSpringSolutionSet;
+    protected final int maxIterations;
+    protected final SolutionListEvaluator<S> evaluator;
+    protected int iterations;
+    protected List<S> archive;
+    protected final StrengthRawFitnessSDE<S> strenghtRawFitness = new StrengthRawFitnessSDE<>();
+    protected final EnvironmentalSelectionSDE<S> environmentalSelection;
 
-    protected final Problem<S> problem_;
+    public SPEA2SDE(Problem<S> problem, int maxIterations, int populationSize,
+            CrossoverOperator<S> crossoverOperator, MutationOperator<S> mutationOperator,
+            SelectionOperator<List<S>, S> selectionOperator, SolutionListEvaluator<S> evaluator) {
+        super(problem);
+        this.maxIterations = maxIterations;
+        this.setMaxPopulationSize(populationSize);
 
-    public SPEA2SDE(SPEA2SDEBuilder<S> builder) {
-        problem_ = builder.getProblem();
-        populationSize = builder.getPopulationSize();
-        archiveSize = builder.getArchiveSize();
-        maxEvaluations = builder.getMaxEvaluations();
-        crossoverOperator = builder.getCrossover();
-        mutationOperator = builder.getMutation();
-        selectionOperator = builder.getSelection();
+        this.crossoverOperator = crossoverOperator;
+        this.mutationOperator = mutationOperator;
+        this.selectionOperator = selectionOperator;
+        this.environmentalSelection = new EnvironmentalSelectionSDE<>(populationSize);
+
+        this.archive = new ArrayList<>(populationSize);
+
+        this.evaluator = evaluator;
     }
 
     @Override
-    public void run() {
-        solutionSet = new ArrayList<>(populationSize);
-        archive = new ArrayList<>(archiveSize);
-        evaluations = 0;
+    protected void initProgress() {
+        iterations = 1;
+    }
 
-        for (int i = 0; i < populationSize; i++) {
-            S newSolution = problem_.createSolution();
-            problem_.evaluate(newSolution);
-            evaluations++;
-            solutionSet.add(newSolution);
+    @Override
+    protected void updateProgress() {
+        iterations++;
+    }
+
+    @Override
+    protected boolean isStoppingConditionReached() {
+        return iterations >= maxIterations;
+    }
+
+    @Override
+    protected List<S> evaluatePopulation(List<S> population) {
+        population = evaluator.evaluate(population, getProblem());
+        return population;
+    }
+
+    @Override
+    protected List<S> selection(List<S> population) {
+        List<S> union = new ArrayList<>(2 * getMaxPopulationSize());
+        union.addAll(archive);
+        union.addAll(population);
+        strenghtRawFitness.computeDensityEstimator(union);
+        archive = environmentalSelection.execute(union);
+        return archive;
+    }
+
+    @Override
+    protected List<S> reproduction(List<S> population) {
+        List<S> offSpringPopulation = new ArrayList<>(getMaxPopulationSize());
+
+        while (offSpringPopulation.size() < getMaxPopulationSize()) {
+            List<S> parents = new ArrayList<>(2);
+            S candidateFirstParent = selectionOperator.execute(population);
+            parents.add(candidateFirstParent);
+            S candidateSecondParent;
+            candidateSecondParent = selectionOperator.execute(population);
+            parents.add(candidateSecondParent);
+
+            List<S> offspring = crossoverOperator.execute(parents);
+            mutationOperator.execute(offspring.get(0));
+            offSpringPopulation.add(offspring.get(0));
         }
+        return offSpringPopulation;
+    }
 
-        while (evaluations < maxEvaluations) {
-            List<S> union = new ArrayList<>(populationSize + archiveSize);
-            union.addAll(solutionSet);
-            union.addAll(archive);
-
-            SPEA2SDEFitness spea = new SPEA2SDEFitness(union);
-            spea.fitnessAssign();
-            archive = spea.environmentalSelection(archiveSize);
-            // Create a new offspringPopulation
-            offSpringSolutionSet = new ArrayList<>(populationSize);
-            List<S> parents = new ArrayList<>();
-            while (offSpringSolutionSet.size() < populationSize) {
-                int j = 0;
-                do {
-                    j++;
-                    parents.add(selectionOperator.execute(archive));
-                } while (j < SPEA2SDE.TOURNAMENTS_ROUNDS); // do-while
-                int k = 0;
-                do {
-                    k++;
-                    parents.add(selectionOperator.execute(archive));
-                } while (k < SPEA2SDE.TOURNAMENTS_ROUNDS); // do-while
-
-                // make the crossover
-                List<S> offSpring = crossoverOperator.execute(parents);
-                mutationOperator.execute(offSpring.get(0));
-                problem_.evaluate(offSpring.get(0));
-                offSpringSolutionSet.add(offSpring.get(0));
-                evaluations++;
-            } // while
-            // End Create a offSpring solutionSet
-            solutionSet = offSpringSolutionSet;
-
-        } // while
-
+    @Override
+    protected List<S> replacement(List<S> population,
+            List<S> offspringPopulation) {
+        return offspringPopulation;
     }
 
     @Override
     public List<S> getResult() {
-        return SolutionListUtils.getNondominatedSolutions(solutionSet);
+        return archive;
     }
 
     @Override
     public String getName() {
-        return "SPEA2SDE";
+        return "SPEA2";
     }
 
     @Override
     public String getDescription() {
-        return "SPEA2 with Shift-Based Density Estimation";
+        return "Strength Pareto. Evolutionary Algorithm";
     }
-
 }
