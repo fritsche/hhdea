e3e9f79707e8f8958a2379437d2a3095e0d119e1
Gian Fritsche <gian.fritsche@gmail.com>
2018-09-03 14:27:56 -0300

    R2 fitness improvement rate using ASF as utility function

--- HHdEA/src/main/java/br/ufpr/inf/cbio/hhdea/metrics/fir/R2ASFFIR.java
+++ HHdEA/src/main/java/br/ufpr/inf/cbio/hhdea/metrics/fir/R2ASFFIR.java
@@ -17,7 +17,7 @@
 package br.ufpr.inf.cbio.hhdea.metrics.fir;
 
 import br.ufpr.inf.cbio.hhdea.metrics.indicator.R2;
-import br.ufpr.inf.cbio.hhdea.metrics.utilityfunction.Tchebycheff;
+import br.ufpr.inf.cbio.hhdea.metrics.utilityfunction.ASF;
 import br.ufpr.inf.cbio.hhdea.util.WeightVectorUtils;
 import java.util.List;
 import org.uma.jmetal.problem.Problem;
@@ -29,12 +29,12 @@ import org.uma.jmetal.util.front.imp.ArrayFront;
  *
  * @author Gian Fritsche <gmfritsche at inf.ufpr.br>
  */
-public class R2TchebycheffFIR implements FitnessImprovementRate<Solution<?>> {
+public class R2ASFFIR implements FitnessImprovementRate<Solution<?>> {
 
     private final double[][] lambda;
     private final int m;
 
-    public R2TchebycheffFIR(Problem problem, int populationSize) {
+    public R2ASFFIR(Problem problem, int populationSize) {
         this.lambda = WeightVectorUtils.initializeUniformWeight(problem, populationSize);
         this.m = problem.getNumberOfObjectives();
     }
@@ -52,7 +52,7 @@ public class R2TchebycheffFIR implements FitnessImprovementRate<Solution<?>> {
         for (int o = 0; o < offspring.size(); o++, i++) {
             reference.setPoint(i, offspringFront.getPoint(o));
         }
-        R2 r2 = new R2(lambda, reference, new Tchebycheff());
+        R2 r2 = new R2(lambda, reference, new ASF());
         double parentR2 = r2.r2(parentsFront);
         double offspringR2 = r2.r2(offspringFront);
         return (parentR2 - offspringR2) / offspringR2;
