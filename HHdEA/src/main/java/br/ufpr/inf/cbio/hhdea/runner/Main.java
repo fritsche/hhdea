01ada6de933bf2252951001b9598f50a30640a32
Gian Fritsche <gian.fritsche@gmail.com>
2018-09-25 09:42:30 -0300

    main parses cli and run runner

--- HHdEA/src/main/java/br/ufpr/inf/cbio/hhdea/runner/Main.java
+++ HHdEA/src/main/java/br/ufpr/inf/cbio/hhdea/runner/Main.java
@@ -1,13 +1,5 @@
 package br.ufpr.inf.cbio.hhdea.runner;
 
-import br.ufpr.inf.cbio.hhdea.config.AlgorithmConfigurationFactory;
-import br.ufpr.inf.cbio.hhdea.problem.ProblemFactory;
-import br.ufpr.inf.cbio.hhdea.runner.methodology.ArionMethodology;
-import br.ufpr.inf.cbio.hhdea.runner.methodology.MaFMethodology;
-import br.ufpr.inf.cbio.hhdea.runner.methodology.Methodology;
-import br.ufpr.inf.cbio.hhdea.runner.methodology.NSGAIIIMethodology;
-import br.ufpr.inf.cbio.hhdea.util.OutputUtils;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.cli.CommandLine;
@@ -17,17 +9,8 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-import org.uma.jmetal.algorithm.Algorithm;
-import org.uma.jmetal.algorithm.multiobjective.moead.util.MOEADUtils;
-import org.uma.jmetal.problem.Problem;
-import org.uma.jmetal.solution.DoubleSolution;
-import org.uma.jmetal.util.AlgorithmRunner;
 import org.uma.jmetal.util.JMetalException;
 import org.uma.jmetal.util.JMetalLogger;
-import org.uma.jmetal.util.SolutionListUtils;
-import org.uma.jmetal.util.fileoutput.SolutionListOutput;
-import org.uma.jmetal.util.fileoutput.impl.DefaultFileOutputContext;
-import org.uma.jmetal.util.pseudorandom.JMetalRandom;
 
 /*
  * Copyright (C) 2018 Gian Fritsche <gmfritsche at inf.ufpr.br>
@@ -51,132 +34,83 @@ import org.uma.jmetal.util.pseudorandom.JMetalRandom;
  */
 public class Main {
 
-    public static void main(String[] args) {
-
-        JMetalLogger.logger.setLevel(Level.ALL);
+    public static CommandLine parse(String[] args) throws ParseException {
         CommandLineParser parser = new DefaultParser();
         Options options = new Options();
-        Option h = Option.builder("h").longOpt("help")
-                .desc("print this message").build();
-        options.addOption(h);
-        Option P = Option.builder("P").longOpt("output-path").hasArg().argName("path")
-                .desc("directory path for output (if no path is given experiment/ will be used.)").build();
-        options.addOption(P);
-        Option M = Option.builder("M").longOpt("methodology").hasArg().argName("methodology")
-                .desc("set the methodology to be used: NSGAIII (default), MaF, Arion.").build();
-        options.addOption(M);
-        Option a = Option.builder("a").longOpt("algorithm").hasArg().argName("algorithm").required()
+
+        options.addOption(Option.builder("h").longOpt("help").desc("print this message").build());
+
+        options.addOption(Option.builder("P").longOpt("output-path").hasArg().argName("path")
+                .desc("directory path for output (if no path is given experiment/ will be used.)").build());
+
+        options.addOption(Option.builder("M").longOpt("methodology").hasArg().argName("methodology")
+                .desc("set the methodology to be used: NSGAIII (default), MaF, Arion.").build());
+
+        options.addOption(Option.builder("a").longOpt("algorithm").hasArg().argName("algorithm").required()
                 .desc("set the algorithm to be executed: NSGAII, MOEAD, MOEADD, ThetaDEA, NSGAIII, SPEA2, SPEA2SDE, HypE, MOMBI2, Traditional, <other>."
-                        + "If <other> name is given, HHdEA will be executed and the algorithm output name will be <other>.").build();
-        options.addOption(a);
-        Option p = Option.builder("p").longOpt("problem").hasArg().argName("problem").required()
+                        + "If <other> name is given, HHdEA will be executed and the algorithm output name will be <other>.").build());
+
+        options.addOption(Option.builder("p").longOpt("problem").hasArg().argName("problem").required()
                 .desc("set the problem instance: DTLZ[1-7], WFG[1-9], MinusDTLZ[1-7], MinusWFG[1-9], MaF[1-15]; "
-                        + "--methodology must be set accordingly.").build();
-        options.addOption(p);
-        Option m = Option.builder("m").longOpt("objectives").hasArg().argName("objectives").required()
-                .desc("set the number of objectives to <objectives>, --problem and --methodology must be set acordingly.").build();
-        options.addOption(m);
-        Option idoption = Option.builder("id").hasArg().argName("id")
-                .desc("independent run id, default 0.").build();
-        options.addOption(idoption);
-        Option seedOption = Option.builder("s").longOpt("seed").hasArg().argName("seed")
-                .desc("set the seed for JMetalRandom, default System.currentTimeMillis()").build();
-        options.addOption(seedOption);
+                        + "--methodology must be set accordingly.").build());
 
-        String aux;
-        String experimentBaseDirectory = "experiment/";
-        String methodologyName = "NSGAIIIMethodology";
-        String algorithmName;
-        String problemName;
-        int numberOfObjectives;
-        int id = 0;
-        long seed = System.currentTimeMillis();
+        options.addOption(Option.builder("m").longOpt("objectives").hasArg().argName("objectives").required()
+                .desc("set the number of objectives to <objectives>, --problem and --methodology must be set acordingly.").build());
 
+        options.addOption(Option.builder("id").hasArg().argName("id")
+                .desc("independent run id, default 0.").build());
+
+        options.addOption(Option.builder("s").longOpt("seed").hasArg().argName("seed")
+                .desc("set the seed for JMetalRandom, default System.currentTimeMillis()").build());
+
+        CommandLine cmd = parser.parse(options, args);
+        if (cmd.hasOption("h")) {
+            // automatically generate the help statement
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("hhdea", options);
+        }
+        return cmd;
+    }
+
+    public static Runner getRunner(CommandLine cmd) {
+        String algorithmName, problemName, objectives, idStr, seedStr, experimentBaseDirectory, methodologyName;
+
+        // required arguments
+        if ((algorithmName = cmd.getOptionValue("a")) == null) {
+            throw new JMetalException("Parameter -a is required");
+        }
+        if ((problemName = cmd.getOptionValue("p")) == null) {
+            throw new JMetalException("Parameter -p is required");
+        }
+        if ((objectives = cmd.getOptionValue("m")) == null) {
+            throw new JMetalException("Parameter -m is required");
+        }
+        Runner runner = new Runner(algorithmName, problemName, Integer.parseInt(objectives));
+
+        // optional arguments
+        if ((experimentBaseDirectory = cmd.getOptionValue("P")) != null) {
+            runner.setExperimentBaseDirectory(experimentBaseDirectory);
+        }
+        if ((idStr = cmd.getOptionValue("id")) != null) {
+            runner.setId(Integer.parseInt(idStr));
+        }
+        if ((methodologyName = cmd.getOptionValue("M")) != null) {
+            runner.setMethodologyName(methodologyName);
+        }
+        if ((seedStr = cmd.getOptionValue("s")) != null) {
+            runner.setSeed(Long.parseLong(seedStr));
+        }
+        return runner;
+    }
+
+    public static void main(String[] args) {
+        JMetalLogger.logger.setLevel(Level.ALL);
         try {
-            CommandLine cmd = parser.parse(options, args);
-            if (cmd.hasOption(h.getOpt())) {
-                // automatically generate the help statement
-                HelpFormatter formatter = new HelpFormatter();
-                formatter.printHelp("hhdea", options);
-            }
-            if ((aux = cmd.getOptionValue(P.getOpt())) != null) {
-                experimentBaseDirectory = aux;
-            }
-            if ((aux = cmd.getOptionValue(M.getOpt())) != null) {
-                methodologyName = aux + "Methodology";
-            }
-            if ((aux = cmd.getOptionValue(a.getOpt())) != null) {
-                algorithmName = aux;
-            } else if (a.isRequired()) {
-                throw new JMetalException("Parameter -" + a.getOpt() + " is required");
-            }
-            if ((aux = cmd.getOptionValue(p.getOpt())) != null) {
-                problemName = aux;
-                if ((aux = cmd.getOptionValue(m.getOpt())) != null) {
-                    problemName = aux;
-                }
-            } else if (p.isRequired()) {
-                throw new JMetalException("Parameter -" + p.getOpt() + " is required");
-            }
+            Runner runner = getRunner(parse(args));
+            runner.run();
+            runner.printResult();
         } catch (ParseException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        Problem problem = ProblemFactory.getProblem(problemName, m);
-
-        Methodology methodology = null;
-        if (methodologyName.equals(NSGAIIIMethodology.class.getSimpleName())) {
-            methodology = new NSGAIIIMethodology(problemName, m);
-        } else if (methodologyName.equals(MaFMethodology.class.getSimpleName())) {
-            methodology = new MaFMethodology(m, problem.getNumberOfVariables());
-        } else if (methodologyName.equals(ArionMethodology.class.getSimpleName())) {
-            methodology = new ArionMethodology(problemName);
-        } else {
-            throw new JMetalException("There is no configuration for " + methodologyName + " methodology.");
-        }
-
-        int maxFitnessevaluations = methodology.getMaxFitnessEvaluations();
-        int popSize = methodology.getPopulationSize();
-
-        // set seed
-        JMetalRandom.getInstance().setSeed(seed);
-
-        Algorithm<List<DoubleSolution>> algorithm = AlgorithmConfigurationFactory
-                .getAlgorithmConfiguration(algorithmName)
-                .configure(popSize, maxFitnessevaluations, problem);
-
-        AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm)
-                .execute();
-
-        List population = SolutionListUtils.getNondominatedSolutions(algorithm.getResult());
-
-        // final population size of MaF
-        if (problem.getName().startsWith("MaF")) {
-            popSize = 240;
-        }
-
-        // prune output population size
-        if (population.size() > popSize) {
-            population = MOEADUtils.getSubsetOfEvenlyDistributedSolutions(population, popSize);
-        }
-
-        String folder = experimentBaseDirectory + "/"
-                + methodologyName + "/"
-                + m
-                + "/data/"
-                + algorithmName + "/"
-                + problemName + "/";
-
-        OutputUtils outputUtils = new OutputUtils(folder);
-        outputUtils.prepareOutputDirectory();
-
-        new SolutionListOutput(population).setSeparator("\t")
-                .setVarFileOutputContext(new DefaultFileOutputContext(folder + "VAR" + id + ".tsv"))
-                .setFunFileOutputContext(new DefaultFileOutputContext(folder + "FUN" + id + ".tsv"))
-                .print();
-
-        long computingTime = algorithmRunner.getComputingTime();
-        JMetalLogger.logger.log(Level.INFO, "Total execution time: {0}ms", computingTime);
-
     }
 }
--- HHdEA/src/main/java/br/ufpr/inf/cbio/hhdea/runner/Runner.java
+++ HHdEA/src/main/java/br/ufpr/inf/cbio/hhdea/runner/Runner.java
@@ -1,5 +1,28 @@
+package br.ufpr.inf.cbio.hhdea.runner;
+
+import br.ufpr.inf.cbio.hhdea.config.AlgorithmConfigurationFactory;
+import br.ufpr.inf.cbio.hhdea.problem.ProblemFactory;
+import br.ufpr.inf.cbio.hhdea.runner.methodology.ArionMethodology;
+import br.ufpr.inf.cbio.hhdea.runner.methodology.MaFMethodology;
+import br.ufpr.inf.cbio.hhdea.runner.methodology.Methodology;
+import br.ufpr.inf.cbio.hhdea.runner.methodology.NSGAIIIMethodology;
+import br.ufpr.inf.cbio.hhdea.util.OutputUtils;
+import java.util.List;
+import java.util.logging.Level;
+import org.uma.jmetal.algorithm.Algorithm;
+import org.uma.jmetal.algorithm.multiobjective.moead.util.MOEADUtils;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.solution.DoubleSolution;
+import org.uma.jmetal.util.AlgorithmRunner;
+import org.uma.jmetal.util.JMetalException;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.SolutionListUtils;
+import org.uma.jmetal.util.fileoutput.SolutionListOutput;
+import org.uma.jmetal.util.fileoutput.impl.DefaultFileOutputContext;
+import org.uma.jmetal.util.pseudorandom.JMetalRandom;
+
 /*
- * Copyright (C) 2018 Gian Fritsche <gmfritsche@inf.ufpr.br>
+ * Copyright (C) 2018 Gian Fritsche <gmfritsche at inf.ufpr.br>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,27 +37,82 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package br.ufpr.inf.cbio.hhdea.runner;
-
-import br.ufpr.inf.cbio.hhdea.util.OutputUtils;
-import java.util.List;
-import java.util.logging.Level;
-import org.uma.jmetal.algorithm.multiobjective.moead.util.MOEADUtils;
-import org.uma.jmetal.util.AlgorithmRunner;
-import org.uma.jmetal.util.JMetalLogger;
-import org.uma.jmetal.util.SolutionListUtils;
-import org.uma.jmetal.util.fileoutput.SolutionListOutput;
-import org.uma.jmetal.util.fileoutput.impl.DefaultFileOutputContext;
-
 /**
  *
- * @author Gian Fritsche <gmfritsche@inf.ufpr.br>
+ * @author Gian Fritsche <gmfritsche at inf.ufpr.br>
  */
 public class Runner {
 
-    void run() {
+    private String experimentBaseDirectory;
+    private String methodologyName;
+    private final String algorithmName;
+    private final String problemName;
+    private final int m;
+    private int id;
+    private long seed;
+
+    private Algorithm<List<DoubleSolution>> algorithm;
+    private Problem problem;
+    private int popSize;
+
+    public Runner(String algorithmName, String problemName, int m) {
+        // required parameters
+        this.algorithmName = algorithmName;
+        this.problemName = problemName;
+        this.m = m;
+
+        // required parameters with default values
+        this.experimentBaseDirectory = "experiment/";
+        this.methodologyName = NSGAIIIMethodology.class.getSimpleName();
+        this.id = 0;
+        this.seed = System.currentTimeMillis();
+    }
+
+    public Runner(String experimentBaseDirectory, String methodologyName,
+            String algorithmName, String problemName, int m, int id, long seed) {
+        this.experimentBaseDirectory = experimentBaseDirectory;
+        this.methodologyName = methodologyName;
+        this.algorithmName = algorithmName;
+        this.problemName = problemName;
+        this.m = m;
+        this.id = id;
+        this.seed = seed;
+    }
+
+    public void run() {
+
+        problem = ProblemFactory.getProblem(problemName, m);
+
+        Methodology methodology = null;
+        if (methodologyName.equals(NSGAIIIMethodology.class.getSimpleName())) {
+            methodology = new NSGAIIIMethodology(problemName, m);
+        } else if (methodologyName.equals(MaFMethodology.class.getSimpleName())) {
+            methodology = new MaFMethodology(m, problem.getNumberOfVariables());
+        } else if (methodologyName.equals(ArionMethodology.class.getSimpleName())) {
+            methodology = new ArionMethodology(problemName);
+        } else {
+            throw new JMetalException("There is no configuration for " + methodologyName + " methodology.");
+        }
+
+        int maxFitnessevaluations = methodology.getMaxFitnessEvaluations();
+        popSize = methodology.getPopulationSize();
+
+        // set seed
+        JMetalRandom.getInstance().setSeed(seed);
+
+        algorithm = AlgorithmConfigurationFactory
+                .getAlgorithmConfiguration(algorithmName)
+                .configure(popSize, maxFitnessevaluations, problem);
+
         AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm)
                 .execute();
+
+        long computingTime = algorithmRunner.getComputingTime();
+        JMetalLogger.logger.log(Level.INFO, "Total execution time: {0}ms", computingTime);
+
+    }
+
+    public void printResult() {
 
         List population = SolutionListUtils.getNondominatedSolutions(algorithm.getResult());
 
@@ -62,9 +140,26 @@ public class Runner {
                 .setVarFileOutputContext(new DefaultFileOutputContext(folder + "VAR" + id + ".tsv"))
                 .setFunFileOutputContext(new DefaultFileOutputContext(folder + "FUN" + id + ".tsv"))
                 .print();
+    }
 
-        long computingTime = algorithmRunner.getComputingTime();
-        JMetalLogger.logger.log(Level.INFO, "Total execution time: {0}ms", computingTime);
+    public Runner setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public Runner setSeed(long seed) {
+        this.seed = seed;
+        return this;
+    }
+
+    public Runner setExperimentBaseDirectory(String experimentBaseDirectory) {
+        this.experimentBaseDirectory = experimentBaseDirectory;
+        return this;
+    }
+
+    public Runner setMethodologyName(String methodologyName) {
+        this.methodologyName = methodologyName;
+        return this;
     }
 
 }
