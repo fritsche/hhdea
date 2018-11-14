d0ab982749aa2ba6a9f15546ffeb5135732295b0
Gian Fritsche <gian.fritsche@gmail.com>
2018-11-14 11:18:38 -0200

    organization and fir scripts

--- scripts/join.sh
+++ scripts/join.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-filename="selected"
+basename=$1
 begin=0
 end=19
 interval="$begin..$end"
@@ -9,6 +9,7 @@ path=experiment/MaFMethodology/
 for m in 5 10 15; do
 	for p in `seq -w 1 15`; do
 		output=$path/$m/output/HHdEA2/MaF$p/
-		eval "paste $output/$filename.{$interval} | column -s $'\t' -t > $output/$filename"
+		echo $output
+		eval "paste $output/$basename.{$interval} | column -s $'\t' -t > $output/$basename"
 	done
 done
